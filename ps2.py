fea9957294c881f8ee8f75c487197ca5b550327d
2018JTM2245 <nehansit151@gmail.com>
2018-09-27 15:15:45 +0530

    updated

--- ps2.py
+++ ps2.py
@@ -1,3 +1,20 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+st=raw_input("enter the string")
+k1=int(input("enter key1="))
+k2=int(input("enter key2="))
+k3=int(input("enter key3="))
+l=len(st)
+import array as arr
+a1=array('c',[])
+a2=array('c',[])
+a3=array('c',[])
+for i in range(l):
+   if(st[i]>='a' and st[i]<='i'):
+      a1.append(st[i])
+   if(st(i)>='j' and st(i)<='r'):
+      a2.append(st(i))
+   if(st(i)>='s' and st(i)<='z' and st(i)=='_'):
+      a3.append(st(i))
+
